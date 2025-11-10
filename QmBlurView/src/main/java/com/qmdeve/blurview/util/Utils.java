64b92b24a512c16317a56bc229d3b71021ab6e37
QmDeve <qmdeve@outlook.com>
2025-11-10 20:31:57 +0800

    Update Utils.java

--- QmBlurView/src/main/java/com/qmdeve/blurview/util/Utils.java
+++ QmBlurView/src/main/java/com/qmdeve/blurview/util/Utils.java
@@ -1,6 +1,8 @@
 package com.qmdeve.blurview.util;
 
 import android.content.res.Resources;
+import android.graphics.Path;
+import android.graphics.RectF;
 import android.util.TypedValue;
 import android.view.View;
 
@@ -20,5 +22,37 @@ public class Utils {
             return navigationBars.bottom;
         }
         return 0;
+    }
+
+    public static void roundedRectPath(RectF rect, float radius, Path path) {
+        path.reset();
+
+        if (radius <= 0) {
+            path.addRect(rect, Path.Direction.CW);
+            return;
+        }
+
+        float maxRadius = Math.min(rect.width(), rect.height()) / 2f;
+        radius = Math.min(radius, maxRadius);
+        float controlOffset = radius * 0.5522847498f;
+
+        path.moveTo(rect.left + radius, rect.top);
+        path.lineTo(rect.right - radius, rect.top);
+        path.cubicTo(rect.right - radius + controlOffset, rect.top,
+                rect.right, rect.top + radius - controlOffset,
+                rect.right, rect.top + radius);
+        path.lineTo(rect.right, rect.bottom - radius);
+        path.cubicTo(rect.right, rect.bottom - radius + controlOffset,
+                rect.right - radius + controlOffset, rect.bottom,
+                rect.right - radius, rect.bottom);
+        path.lineTo(rect.left + radius, rect.bottom);
+        path.cubicTo(rect.left + radius - controlOffset, rect.bottom,
+                rect.left, rect.bottom - radius + controlOffset,
+                rect.left, rect.bottom - radius);
+        path.lineTo(rect.left, rect.top + radius);
+        path.cubicTo(rect.left, rect.top + radius - controlOffset,
+                rect.left + radius - controlOffset, rect.top,
+                rect.left + radius, rect.top);
+        path.close();
     }
 }
