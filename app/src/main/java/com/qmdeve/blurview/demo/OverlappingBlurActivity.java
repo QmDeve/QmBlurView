5e0387f85640e4dff4130f133d70d3c38a84c3f6
QmDeve <me@qmdeve.com>
2025-11-28 04:53:45 +0800

    OverlappingBlurActivity Add immersive

--- app/src/main/java/com/qmdeve/blurview/demo/OverlappingBlurActivity.java
+++ app/src/main/java/com/qmdeve/blurview/demo/OverlappingBlurActivity.java
@@ -2,13 +2,20 @@ package com.qmdeve.blurview.demo;
 
 import android.os.Bundle;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.qmdeve.blurview.demo.util.Utils;
 
 public class OverlappingBlurActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_overlapping_blur);
+
+        Utils.transparentStatusBar(getWindow());
+        Utils.transparentNavigationBar(getWindow());
     }
 }
